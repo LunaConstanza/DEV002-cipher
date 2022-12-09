3630c9360366ae0dd83f3a5266b1086f26b1fa0f
LunaConstanza <lunagonb@gmail.com>
2022-12-09 20:19:02 -0300

    creación de métodos encode y decode + funcionalidad parcial

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,66 @@
 const cipher = {
-  // ...
+  encode: function(offset, string){
+  // --------------- PRIMERA OPCIÓN UTILIZANDO ABECEDARIO ----------------
+    string = string.toUpperCase();
+    offset = parseInt(offset)
+    const abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let finalString = '';
+    for(let i = 0; i < abc.length; i++){     // hay error en el orden que entra el cifrado
+      if(string.includes(abc[i])){
+        console.log();
+        let newPosition = (i + offset) % 26;
+        console.log(newPosition);
+        finalString += abc[newPosition];
+      }
+    }
+    console.log(finalString);
+    return finalString;
+  },
+  decode: function(offset, string){
+    // --------------- PRIMERA OPCIÓN UTILIZANDO ABECEDARIO ----------------
+    string = string.toUpperCase();
+    offset = parseInt(offset)
+    const abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let finalString = '';
+    for(let i = 0; i < abc.length; i++){     // hay error en el orden que entra el decifrado
+      if(string.includes(abc[i])){
+        console.log(i);
+        let newPosition = ((i - offset) + 26) % 26;
+        console.log(newPosition);
+        finalString += abc[newPosition];
+      }
+    }
+    console.log(finalString);
+    return finalString;
+  }
 };
+
+
+
+// Abecedario normal
+// (x + n) % 26
+// (posicion letra + valor fijo) residuo de 26
+// (0 + 33) % 26
+// 7
+
+// Abecedario segun ASCII
+// (x - 65 + n) % 26 + 65
+// (n° de la letra ASCII - 65 + valor fijo) residuo de 26 + para obtener código ASCII
+// para encontrar ubicación en el alfabeto
+// (65 - 65 + 33) % 26 + 65
+// 7 + 65
+// 72
+
+// const string = (texto) => {
+//   const letraASCII1 = texto.charCodeAt(0);
+//   console.log('1 resultado A = ',letraASCII1);
+//   const suma = letraASCII1 + 7;
+//   console.log(suma);
+//   const letraASCII2 = String.fromCharCode(suma);
+//   console.log('2 resultado A = ',letraASCII2);
+// }
+// string('ABC');
+
+
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const inputCode = document.getElementById('inputCode');
+const inputDecode = document.getElementById('inputDecode');
+const inputOffset = document.getElementById('offset');
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+    const string1 = inputCode.value;
+    const string2 = inputDecode.value;
+    const offset = inputOffset.value;
+    if (string1 && string2) {
+        alert('Escriba en solo un cuadro de texto.')
+    } else if (string1 && string2 == '') {
+        const resultEncode = cipher.encode(offset, string1);
+        inputDecode.value = resultEncode;
+        inputCode.value = '';
+    } else if (string2 && string1 == '') {
+        const resultDecode = cipher.decode(offset, string2);
+        inputCode.value = resultDecode;
+        inputDecode.value = '';
+    }
+})
+
